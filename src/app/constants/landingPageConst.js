3f308b524c8a8ebb835f978f1cafab6bee15e08a
Maharshi Patel <102695974+Maharshi2403@users.noreply.github.com>
2025-06-19 20:08:31 -0400

    first commit

---
+++ src/app/constants/landingPageConst.js
@@ -0,0 +1,5 @@
+const descriptions = {
+    intro: "HuggingPack is a powerful platform designed to streamline the deployment process of Hugging Face models by automatically generating ready-to-use pipelines. With just a few clicks, users can select a model from the Hugging Face Hub, and HuggingPack builds a complete, production-ready zip package that includes a FastAPI server, Dockerfile, and inference code—all tailored to the selected model. Whether you're a researcher, developer, or ML enthusiast, HuggingPack eliminates the tedious setup process and enables rapid integration of state-of-the-art models into your applications."
+}
+
+export default descriptions;
